e834813bc41bb852e10cba19da1a0b5f36d14fed
Zhengyan Hu <91642985+Gloaming02@users.noreply.github.com>
2024-04-18 15:55:43 -0700

    Create 485MaxConsecutiveOnes.py

---
+++ 485MaxConsecutiveOnes.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        dp = [0] * len(nums)
+        max_count = 0
+        if nums[0] == 0:
+            dp[0] = 0
+        else:
+            dp[0] = 1
+            max_count = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] == 1:
+                dp[i] = dp[i - 1] + 1
+                max_count = max(dp[i], max_count)
+            else:
+                dp[i] = 0
+        return max_count
