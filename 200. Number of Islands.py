afba35c5e00a0812168f55d6c5026f7a3327aef0
dongyinli <dongyinli@hotmail.com>
2024-04-19 17:49:09 -0700

    Format correction

--- 200. Number of Islands.py
+++ 200. Number of Islands.py
@@ -1,31 +1,31 @@
-200. Number of Islands
-Medium
-Topics
-Companies
-Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+# 200. Number of Islands
+# Medium
+# Topics
+# Companies
+# Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 
-An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+# An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 
  
 
-Example 1:
+# Example 1:
 
-Input: grid = [
-  ["1","1","1","1","0"],
-  ["1","1","0","1","0"],
-  ["1","1","0","0","0"],
-  ["0","0","0","0","0"]
-]
-Output: 1
-Example 2:
+# Input: grid = [
+#   ["1","1","1","1","0"],
+#   ["1","1","0","1","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","0","0","0"]
+# ]
+# Output: 1
+# Example 2:
 
-Input: grid = [
-  ["1","1","0","0","0"],
-  ["1","1","0","0","0"],
-  ["0","0","1","0","0"],
-  ["0","0","0","1","1"]
-]
-Output: 3
+# Input: grid = [
+#   ["1","1","0","0","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","1","0","0"],
+#   ["0","0","0","1","1"]
+# ]
+# Output: 3
 
 
 class Solution:
@@ -69,29 +69,29 @@ class SolutionBFS:
         visited=set()
         islands=0
 
-         def bfs(r,c):
-             q = deque()
-             visited.add((r,c))
-             q.append((r,c))
-           
-             while q:
-                 row,col = q.popleft()
-                 directions= [[1,0],[-1,0],[0,1],[0,-1]]
-               
-                 for dr,dc in directions:
-                     r,c = row + dr, col + dc
-                     if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r ,c) not in visited:
-                       
-                         q.append((r , c ))
-                         visited.add((r, c ))
+        def bfs(r,c):
+            q = deque()
+            visited.add((r,c))
+            q.append((r,c))
+        
+            while q:
+                row,col = q.popleft()
+                directions= [[1,0],[-1,0],[0,1],[0,-1]]
+            
+                for dr,dc in directions:
+                    r,c = row + dr, col + dc
+                    if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r ,c) not in visited:
+                    
+                        q.append((r , c ))
+                        visited.add((r, c ))
 
-         for r in range(rows):
-             for c in range(cols):
-               
-                 if grid[r][c] == "1" and (r,c) not in visited:
-                     bfs(r,c)
-                     islands +=1 
+        for r in range(rows):
+            for c in range(cols):
+            
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    bfs(r,c)
+                    islands +=1 
 
-         return islands
+        return islands
 
 
